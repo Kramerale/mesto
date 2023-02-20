54f5f351e677f828c83855b9e43b81c577ebaa70
Kramerale <aliceshoolz96@gmail.com>
2023-02-21 00:04:13 +0400

    write function to autofill name and occupation in popup

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__occupation');
+let popupName = document.querySelector('.popup__name');
+let popupOccupation = document.querySelector('.popup__occupation');
+
 editButton.addEventListener('click', function () {
     popup.classList.add('popup_opened');
+    popupName.value = profileName.textContent;
+    popupOccupation.value = profileOccupation.textContent;
 });
 
 closeButton.addEventListener('click', function () {
