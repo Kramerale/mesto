2d1d6d6b194c33a7b3711f92668ca3986fbdf5d5
Kramerale <aliceshoolz96@gmail.com>
2023-02-21 00:20:56 +0400

    started to write function to save name and occupation edits

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
-let popupName = document.querySelector('.popup__name');
-let popupOccupation = document.querySelector('.popup__occupation');
+let editButton = document.querySelector('.profile__edit-button');
+
+let popup = document.querySelector('.popup');
+let popupForm = popup.querySelector('.popup__form');
+let popupName = popup.querySelector('.popup__name');
+let popupOccupation = popup.querySelector('.popup__occupation');
+let closeButton = popup.querySelector('.popup__close-button');
 
 editButton.addEventListener('click', function () {
     popup.classList.add('popup_opened');
@@ -16,3 +17,19 @@ editButton.addEventListener('click', function () {
 closeButton.addEventListener('click', function () {
     popup.classList.remove('popup_opened');
 });
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
