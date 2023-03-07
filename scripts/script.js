1dbcb19f644e39b4f9caf34d28d98d800a36c461
Kramerale <aliceshoolz96@gmail.com>
2023-03-07 16:58:21 +0400

    feat: function to create cards from js arr

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardList = document.querySelector('.elements__card-list');
+
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
 let editButton = document.querySelector('.profile__edit-button');
@@ -7,6 +36,18 @@ let popupForm = popup.querySelector('.popup__form');
 let popupName = popup.querySelector('.popup__input_type_name');
 let popupOccupation = popup.querySelector('.popup__input_type_occupation');
 let closeButton = popup.querySelector('.popup__close-button');
+
+function createNewCard (card) {
+  const newCard = document.querySelector('.card-template').content.cloneNode(true);
+  const cardTitle = newCard.querySelector('.card__title');
+  cardTitle.textContent = card.name;
+
+  const cardImage = newCard.querySelector('.card__image');
+  cardImage.setAttribute('src', card.link);
+  cardImage.setAttribute('alt', card.name);
+
+  cardList.append(newCard);
+};
 
 function openPopup () {
   popup.classList.add('popup_opened');
@@ -26,6 +67,8 @@ function handleFormSubmit (evt) {
 
   closePopup ();
 }
+
+initialCards.forEach(createNewCard);
 
 editButton.addEventListener('click', openPopup);
 
